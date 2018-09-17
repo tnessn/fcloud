36ab537557dbedc9dc9983568e95a80f3c353140
huangjinfeng <1005886607@qq.com>
2018-09-17 20:53:27 +0800

    0

---
+++ fcloud-provider/src/main/java/org/fcloud/eureka/provider/controller/HelloController.java
@@ -0,0 +1,28 @@
+package org.fcloud.eureka.provider.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        List<ServiceInstance> instances = client.getInstances("hello-service");
+        for (int i = 0; i < instances.size(); i++) {
+            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+        return "Hello World";
+    }
+}
