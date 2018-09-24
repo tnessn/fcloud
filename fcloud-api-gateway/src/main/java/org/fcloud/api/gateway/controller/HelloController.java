1785255f17b9b2c7c7b788a43caec9101e557441
huangjinfeng <1005886607@qq.com>
2018-09-24 15:44:04 +0800

    0

---
+++ fcloud-api-gateway/src/main/java/org/fcloud/api/gateway/controller/HelloController.java
@@ -0,0 +1,12 @@
+package org.fcloud.api.gateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/local")
+    public String hello() {
+        return "hello api gateway";
+    }
+}
