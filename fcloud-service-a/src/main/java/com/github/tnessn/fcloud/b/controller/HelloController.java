8f23c4da91d137cc40d24bcd908522741eee9b3a
Jeffery <huangjinfeng91@gmail.com>
2021-09-09 20:08:44 +0800

    test

---
+++ fcloud-service-a/src/main/java/com/github/tnessn/fcloud/b/controller/HelloController.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.github.tnessn.fcloud.b.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author huangjinfeng
+ */
+@RestController
+public class HelloController {
+	
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam String name) {
+		return "123";
+	}
+
+}
