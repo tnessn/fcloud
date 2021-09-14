e34a8826e903a48b771ac2958548a37bc5dc47cf
Jeffery <huangjinfeng91@gmail.com>
2021-09-14 10:32:15 +0800

    test

--- fcloud-a/fcloud-a-service/src/main/java/com/github/tnessn/fcloud/a/service/impl/AServiceImpl.java
+++ fcloud-a/fcloud-a-service/src/main/java/com/github/tnessn/fcloud/a/service/impl/AServiceImpl.java
@@ -3,20 +3,16 @@
  */
 package com.github.tnessn.fcloud.a.service.impl;
 
-import org.apache.dubbo.config.annotation.DubboReference;
 import org.apache.dubbo.config.annotation.DubboService;
 
 import com.github.tnessn.fcloud.a.api.AService;
-import com.github.tnessn.fcloud.b.api.BService;
 
 /**
  * @author huangjinfeng
  */
 @DubboService(version = "1.0.0")
 public class AServiceImpl implements AService {
-	
-	@DubboReference(version = "1.0.0")
-	private BService bService;
+
 
 	@Override
 	public String helloA(String name) {
