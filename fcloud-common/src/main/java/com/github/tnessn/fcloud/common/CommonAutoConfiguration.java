4ffc0ffba7771b7856531bd9a977a2768308937c
Jeffery <huangjinfeng91@gmail.com>
2021-09-22 18:01:11 +0800

    test

---
+++ fcloud-common/src/main/java/com/github/tnessn/fcloud/common/CommonAutoConfiguration.java
@@ -0,0 +1,15 @@
+package com.github.tnessn.fcloud.common;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 
+ * @author Jeffery
+ * @since 30.08.2021
+ */
+@Configuration
+@ComponentScan(value="com.hotcoin.common")
+public class CommonAutoConfiguration {
+
+}
