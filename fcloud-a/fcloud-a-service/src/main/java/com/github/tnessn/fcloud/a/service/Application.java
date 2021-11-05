83a533f3189bae472c90b5333a44bdcade88b849
Jeffery <huangjinfeng91@gmail.com>
2021-11-05 13:43:09 +0800

    test

--- fcloud-a/fcloud-a-service/src/main/java/com/github/tnessn/fcloud/a/service/Application.java
+++ fcloud-a/fcloud-a-service/src/main/java/com/github/tnessn/fcloud/a/service/Application.java
@@ -2,10 +2,8 @@ package com.github.tnessn.fcloud.a.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.ImportResource;
 
-@EnableEurekaClient
 @SpringBootApplication
 @ImportResource(locations = "classpath:dubbo.xml")
 public class Application {
--- fcloud-b/fcloud-b-service/src/main/java/com/github/tnessn/fcloud/b/service/Application.java
+++ fcloud-b/fcloud-b-service/src/main/java/com/github/tnessn/fcloud/b/service/Application.java
@@ -2,13 +2,9 @@ package com.github.tnessn.fcloud.b.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ImportResource;
 
-@EnableEurekaClient
 @SpringBootApplication
-@EnableFeignClients(basePackages = {"com.github.tnessn.fcloud.a.api"})
 @ImportResource(locations = "classpath:dubbo.xml")
 public class Application {
 
