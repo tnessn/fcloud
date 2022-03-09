540039064534990c9c2f25b93c64806ea49eeb4e
Jeffery <huangjinfeng91@gmail.com>
2022-03-09 10:12:02 +0800

    test

---
+++ fcloud-common/src/main/java/com/github/tnessn/fcloud/common/filter/DubboMultiLangFilter.java
@@ -0,0 +1,44 @@
+package com.github.tnessn.fcloud.common.filter;
+
+import java.lang.invoke.MethodHandles;
+
+
+import org.apache.dubbo.common.constants.CommonConstants;
+import org.apache.dubbo.common.extension.Activate;
+import org.apache.dubbo.rpc.Filter;
+import org.apache.dubbo.rpc.Invocation;
+import org.apache.dubbo.rpc.Invoker;
+import org.apache.dubbo.rpc.Result;
+import org.apache.dubbo.rpc.RpcContext;
+import org.apache.dubbo.rpc.RpcException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.tnessn.fcloud.common.util.I18NUtils;
+import com.github.tnessn.fcloud.common.util.MyThreadContext;
+
+
+@Activate(group = {CommonConstants.CONSUMER, CommonConstants.PROVIDER})
+public class DubboMultiLangFilter implements Filter {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+    	LOG.error("00000000000000000000000000:{}",RpcContext.getContext().isConsumerSide());
+       //判断是消费者  还是 服务提供者
+        if (RpcContext.getContext().isConsumerSide()) {
+            RpcContext.getContext().setAttachment(I18NUtils.LANG_PARAM, MyThreadContext.getLang());
+        } else {
+            String lang =RpcContext.getContext().getAttachment(I18NUtils.LANG_PARAM);
+            MyThreadContext.setLang(lang);
+        }
+        try {
+            return invoker.invoke(invocation);
+        } finally {
+            if (RpcContext.getContext().isProviderSide()) {
+                MyThreadContext.unload();;
+            }
+        }
+    }
+}
